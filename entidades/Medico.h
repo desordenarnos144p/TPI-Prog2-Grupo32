e72e5a63f8047d8a46d727e97097bd27b20a5923
Almaneu <167228770+ALMANEUWIRTH@users.noreply.github.com>
2026-06-13 19:19:09 -0300

    Update Medico.h

--- entidades/Medico.h
+++ entidades/Medico.h
@@ -1,9 +1,29 @@
-#ifndef MEDICO_H_INCLUDED
-#define MEDICO_H_INCLUDED
-
+#pragma once
 #include "Persona.h"
-#include <iostream>
+class Medico : public Persona
+{
+public:
+    Medico();
+    Medico(const char* dni, const char* nombre,
+    const char* apellido, const char* telefono, bool estado,
+    int idMedico, const char* matricula, int idEspecialidad);
 
-using namespace std;
+    void setIdMedico(int idMedico);
+    int getIdMedico();
 
-#endif // MEDICO_H_INCLUDED
+    void setMatricula(const char* matricula);
+    const char* getMatricula();
+
+    void setIdEspecialidad(int idEspecialidad);
+    int getIdEspecialidad();
+
+    void cargar(const char* matricula = 0, int idMedico = 0);
+    void mostrar();
+
+
+    bool validarMatricula(const char* matricula);
+private:
+    int _idMedico;
+    int _idEspecialidad;
+    char _matricula[50];
+};
