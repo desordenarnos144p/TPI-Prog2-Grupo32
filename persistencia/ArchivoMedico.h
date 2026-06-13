4102fe73dbe859c72410ff1394cfa91c1839c600
Almaneu <167228770+ALMANEUWIRTH@users.noreply.github.com>
2026-06-13 19:20:13 -0300

    Update ArchivoMedico.h

--- persistencia/ArchivoMedico.h
+++ persistencia/ArchivoMedico.h
@@ -1,6 +1,17 @@
-#ifndef ARCHIVOMEDICO_H_INCLUDED
-#define ARCHIVOMEDICO_H_INCLUDED
+#pragma once
+#include "Medico.h"
+class MedicoArchivo{
+public:
+    MedicoArchivo(const char* nombreArchivo = "Medicos.dat");
+    bool guardar(const Medico &reg);
+    Medico leer(int pos);
+    int getCantidadRegistros();
+    bool modificar(const Medico &reg, int pos);
+    int getNuevoId();
 
+    //AYUDANTES
+    int buscarMedico(const char* matricula);
 
-
-#endif // ARCHIVOMEDICO_H_INCLUDED
+    private:
+    char _nombreArchivo[50];
+};
