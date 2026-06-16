78e175cad2b711384a90f75c548aaac698d8a10b
desordenarnos. <delarosafacu2005@gmail.com>
2026-06-16 20:33:01 -0300

    Se desarrollo ManagerPago.cpp/.h

--- controladores/ManagerPago.h
+++ controladores/ManagerPago.h
@@ -1,6 +1,19 @@
-#ifndef MANAGERPAGO_H_INCLUDED
-#define MANAGERPAGO_H_INCLUDED
+#pragma once
+#include "persistencia/ArchivoPago.h"
+#include "../entidades/Pago.h"
 
-
-
-#endif // MANAGERPAGO_H_INCLUDED
+class ManagerPago{
+    private:
+    ArchivoPago _repoPago;
+    public:
+        //CONSTRUCTOR.
+            ManagerPago();
+        //METODOS.
+            bool agregar();
+            bool modificar();
+            bool bajaLogica();
+            Pago buscarPorId(int id);
+            bool existe(int id);
+            void listar();
+            void listarPorFecha();
+};
