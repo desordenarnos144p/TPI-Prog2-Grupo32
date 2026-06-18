bece88ed22c82f8eb4382a25506283c99ad02712
Almaneu <167228770+ALMANEUWIRTH@users.noreply.github.com>
2026-06-18 09:26:02 -0300

    Update ArchivoEspecialidad.h

--- persistencia/ArchivoEspecialidad.h
+++ persistencia/ArchivoEspecialidad.h
@@ -1,1 +1,16 @@
-//PENDIENTE.
+#pragma once
+#include "Especialidad.h"
+class EspecialidadArchivo{
+public:
+    EspecialidadArchivo();
+
+    bool guardar(Especialidad reg);
+    Especialidad leer(int pos);
+    int getCantidadRegistros();
+    bool modificar(const Especialidad &reg, int pos);
+    int getNuevoId();
+
+    int buscarEspecialidad(const char* especialidad);
+private:
+    char _nombreArchivo[50];
+};
