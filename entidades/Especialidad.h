1203b324700dd483ef9c2da7020d2a6d146ebb9e
Almaneu <167228770+ALMANEUWIRTH@users.noreply.github.com>
2026-06-18 09:23:30 -0300

    Update Especialidad.h

--- entidades/Especialidad.h
+++ entidades/Especialidad.h
@@ -1,1 +1,26 @@
-#pragma one
+#pragma once
+
+class Especialidad{
+public:
+    Especialidad();
+    Especialidad(int idEspecialidad, const char* nombre, bool estado);
+
+    void setIdEspecialidad(int idEspecialidad);
+    int getIdEspecialidad();
+
+    void setNombre(const char* nombre);
+    const char* getNombre();
+
+    void setEstado(bool estado);
+    bool getEstado();
+
+    void cargar(const char* especialidad = 0, int id = 0);
+    void mostrar();
+
+    bool validarEspecialidad(const char* especialidad);
+
+private:
+    int _idEspecialidad;
+    char _nombre[50];
+    bool _estado;
+};
